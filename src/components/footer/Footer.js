7bf86aede59b8189ddb5f2ed706fee94345b4c45
Anand Vasu <anandakumarv@gmail.com>
2018-12-08 20:52:20 -0600

    social media link added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,33 @@
 import React,{Component} from 'react';
 import './Footer.css';
+import linkedin from '../../assets/images/linkedin.png';
+import twitter from '../../assets/images/twitter.png';
+import facebook from '../../assets/images/facebook.png';
 
 class Footer extends Component {
+
+    constructor() {
+        super();
+
+        this.socialMediaClick = this.socialMediaClick.bind(this);
+    }
+
+    socialMediaClick(newURLPage) {
+        window.open("http://"+newURLPage, "_blank");
+     }
 
     render () {
 
         return(
-           <div>
-               Copyright © 2018. All rights reserved
+           <div className="fcontainer">
+               <div className="copyRight">
+                    Copyright © 2018. All rights reserved
+                </div>
+               <div className="socialMedia">
+                        <img src={facebook} onClick={() => this.socialMediaClick("www.facebook.com")} />
+                        <img src={twitter} onClick={() => this.socialMediaClick("www.twitter.com")} />
+                        <img src={linkedin} onClick={() => this.socialMediaClick("www.linkedin.com")} />
+               </div>
            </div>     
         );
     }
