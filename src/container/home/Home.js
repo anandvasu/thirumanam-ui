54f7024319e24828a6ef508474c8e1a0956d41d5
Anand Vasu <anandakumarv@gmail.com>
2018-12-08 19:44:08 -0600

    updated width

--- src/container/home/Home.js
+++ src/container/home/Home.js
@@ -21,14 +21,25 @@ class Home extends Component {
                     </div> 
                     <div className="vs100" />
                 </div>
-                <div className="hs10" />
-                <div className="container"> 
-                            <div className="contentLeft">
-                                <img src={mobile} alt="Not Available" />    
-                            </div>
-                            <div className="contentLeft">
-                                <label>100% Verified Mobile Numbers</label>       
-                            </div>
+                <div className="barContainer"> 
+                    <div className="barIContainer">
+                        <div className="barImage">
+                            <img src={mobile} alt="Not Available" />    
+                        </div>
+                        <div className="barText">
+                            <div className="hs20" />
+                            <label>100% Verified Mobile Numbers</label>       
+                        </div>
+                    </div>
+                    <div className="barIContainer">
+                        <div className="barImage">
+                            <img src={mobile} alt="Not Available" />    
+                        </div>
+                        <div className="barText">
+                            <div className="hs20" />
+                            <label>20 Years of Experience</label>       
+                        </div>
+                    </div>
                 </div>
                 <div className="hs20" />
                 <Footer />
